c84ba899df6a5050e7b2af85e81ee3a3e7a81f5a
Nikita Mishenev <clowixdev@gmail.com>
2025-05-16 14:13:29 +0300

    added contact state

--- states/states.py
+++ states/states.py
@@ -2,11 +2,12 @@ from aiogram.fsm.state import StatesGroup, State
 
 class Position(StatesGroup):
     main_menu = State()
+    contact = State()
 
     noti_on_settings = State()
     noti_off_settings = State()
     
-    frequency_setting = State()
+    remind_setting = State()
     dayinfo_setting = State()
     weekends_setting = State()
     visibility_setting = State()
